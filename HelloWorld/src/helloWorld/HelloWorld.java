16c51697363d5f4e8e5ab7b65056b519637ae4ba
JoaoPedroBerwanger <joaopedroberwanger12@gmail.com>
2025-05-23 21:01:40 -0300

    Projeto HelloWorld

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloWorld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) 
+	{
+
+		if(args.length == 0) 
+		{
+			System.out.println("Use: java helloWorld.HelloWorld \"Seu Nome\" ");
+			return;
+		}
+		
+		System.out.println("Olá " + args[0]);
+	
+	}
+}
